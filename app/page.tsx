80fc8518f402fcbbda0e910dee0714301ac3cceb
FIRO <appturbo102@gmail.com>
2024-05-19 18:49:14 +0900

    feat: Add video background to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { useEffect } from 'react';
-
+import style from '../styles/page.module.scss';
 
 export default function Home() {
     return (
         <>
+		<section className={style.container}>
+			<video autoPlay loop muted className={style.video}>
+				<source src='/video/background.mp4' type="video/mp4" />
+			</video>
+		</section>
         </>
     );
 }
