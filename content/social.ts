ced836f26ef0efdb05af4c8aebd8b2625076bffb
FIRO <appturbo102@gmail.com>
2024-06-20 18:48:43 +0900

    feat: Add social links interface and export

---
+++ content/social.ts
@@ -0,0 +1,9 @@
+interface SocialLink {
+    github: string;
+    instagram: string;
+}
+
+export const SocialLink: SocialLink = {
+    github: 'https://github.com/iamfiro',
+    instagram: 'https://www.instagram.com/chxs_u/'
+}
