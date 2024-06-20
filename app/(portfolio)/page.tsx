72d0a3801e1d129a0c6f8a852d4ac0ad7958c654
FIRO <appturbo102@gmail.com>
2024-06-20 19:56:47 +0900

    refactor: Add scroll notification to portfolio hero section

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -1,5 +1,6 @@
 import { HeroTitle } from '@/components';
 import style from '@/styles/portfolio.module.scss';
+import { FiArrowDownCircle } from "react-icons/fi";
 
 export default function Home() {
     return (
@@ -8,7 +9,10 @@ export default function Home() {
             <section className={style.hero}>
                 <div />
                 <HeroTitle />
-                <div />
+                <div className={style.scrollNotification}>
+                    <div>아래로 스크롤 하세요!</div>
+                    <FiArrowDownCircle size={30} />
+                </div>
             </section>
         </main>
         </>
