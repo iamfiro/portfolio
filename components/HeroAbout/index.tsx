a38156ea4b634b56e936cf33741d8961f2a74a30
FIRO <appturbo102@gmail.com>
2024-06-20 20:23:23 +0900

    refactor: Update HeroAbout component in portfolio page

--- components/HeroAbout/index.tsx
+++ components/HeroAbout/index.tsx
@@ -4,10 +4,8 @@ function HeroAbout() {
     return (
         <>
         <section className={style.container}>
-            <div>
-            <div className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg border bg-white dark:bg-black">
-                
-                </div>
+            <div className={style.wrap}>
+            
             </div>
         </section>
         </>
