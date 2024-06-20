0d5e72bba4d4e755f9586c0973a219c448ddc718
FIRO <appturbo102@gmail.com>
2024-06-20 22:06:42 +0900

    refactor: Update HeroStack component with new styles and content

--- components/HeroStack/index.tsx
+++ components/HeroStack/index.tsx
@@ -31,7 +31,15 @@ const slugs = [
 ];
 
 export default function HeroStack() {
-	return <section className={style.container}>
-        <IconCloud iconSlugs={slugs} />
-    </section>;
+	return (
+        <section className={style.container}>
+            <div className={style.top}>
+                <h1 className={style.title}>사용하는 기술</h1>
+                <span className={style.tip}>TIP: 기술에 대한 자세한 정보를 보시려면 아래 아이콘을 눌러주세요</span>
+            </div>
+            <div className={style.iconContainer}>
+                <IconCloud iconSlugs={slugs} />
+            </div>
+        </section>
+    )
 }
