c11e3928d2a291bb1f90cf074d1188b49f9c0f9a
FIRO <appturbo102@gmail.com>
2024-06-20 18:49:00 +0900

    feat: Update header component with new design and social links

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,18 +1,32 @@
 import Image from "next/image";
-import Logo from '../public/logo.png';
-import ShimmerButton from "../shimmer-button";
-import style from './header.module.scss';
+import Logo from '@/public/logo.png';
+import style from './style.module.scss';
+import Link from "next/link";
+// 소셜 아이콘
+import { SocialLink } from "@/content/social";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Header() {
     return (
-        <header className="h-[65px] flex items-center w-full max-w-[1440px] mx-auto px-[15px]">
-            <Image src={Logo} width={25} alt="page logo" />
-            <span className="ml-2 font-bold text-base">개발자 조성주</span>
-            <ShimmerButton className="shadow-2xl">
-                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10">
-                    Github 바로가기
-                </span>
-            </ShimmerButton>
+        <header className={style.container} role="heading">
+            <div className={style.left}>
+                <Image src={Logo} width={25} alt="page logo" />
+                <h1 className={style.name}>개발자 FIRO</h1>
+                <ul className={style.menu}>
+                    <li>
+                        <Link href="#project">프로젝트 리스트</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={style.right}>
+                <Link href={SocialLink.github} className={style.social} target="_blank">
+                    <FaGithub size={18} />
+                </Link>
+                <Link href={SocialLink.instagram} className={style.social} target="_blank">
+                    <FaInstagram size={18} />
+                </Link>
+            </div>
         </header>
     )
 }
