c985d69b7e01e062d7e16fcf2b80f52624274d59
FIRO <appturbo102@gmail.com>
2024-06-20 18:15:06 +0900

    chore: Update package.json with sass dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.scss";
 
 const NotoSansKR = Noto_Sans_KR({
 	weight: ["100", "200", "300", "400", "500", "600", "700"],
