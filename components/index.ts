72dcf04f903adf7bdf9b2e22f2dfc59f62028529
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-06 21:40:06 +0900

    delete all file

--- components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './common/Header';
-export { default as HeroTitle } from './HeroTitle';
-export { default as HeroAbout } from './HeroAbout';
-export { default as HeroStack } from './HeroStack';
-export { default as IconCloud } from './common/IconCloud';
-export { default as HeroNotification } from './common/Notification';
--- content/social.ts
+++
@@ -1,9 +0,0 @@
-interface SocialLink {
-    github: string;
-    instagram: string;
-}
-
-export const SocialLink: SocialLink = {
-    github: 'https://github.com/iamfiro',
-    instagram: 'https://www.instagram.com/chxs_u/'
-}
