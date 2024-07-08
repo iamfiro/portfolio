6455416e813b7bbc03d7c57be28aee7ff0a7a5b4
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-08 16:00:05 +0900

    refactor: Add header navigation links to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,17 @@
 import { LandingHero } from "../components";
+import Header from "../components/common/Header";
 import style from '../styles/page.module.scss';
 function Home() {
     return (
         <>
         <main className={style.main}>
+            <Header>
+                <Header.NavList>
+                    <Header.NavItem>Project</Header.NavItem>
+                    <Header.NavItem>Skills</Header.NavItem>
+                    <Header.NavItem>Contact</Header.NavItem>
+                </Header.NavList>
+            </Header>
             <LandingHero />
         </main>
         </>
