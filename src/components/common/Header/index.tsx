04b854859ab3edb234a45ee043f383e0b93c81ba
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-08 15:54:16 +0900

    refactor: Add Header component with navigation functionality

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Children, ReactNode } from 'react';
+import style from './style.module.scss';
+
+interface HeaderProps {
+    children: ReactNode;
+}
+
+function Header({ children }: HeaderProps) {
+    return (
+        <header className={style.header}>
+            {children}
+        </header>
+    );
+}
+
+Header.NavList = ({ children }: HeaderProps) => <ul className={style.navContainer}>{children}</ul>;
+Header.NavItem = ({ children }: HeaderProps) => <li className={style.navItem}>{children}</li>;
+export default Header;
