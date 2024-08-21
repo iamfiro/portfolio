60cc352f56fe8b71afd9ed7e50d04e81801b7cb5
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-21 22:20:21 +0900

    feat: Custom cursor 개발

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -1,29 +1,49 @@
-import {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-const CustomCursor = () => {
-	const [mousePosition, setMousePosition] = useState({
-		x: 400,
-		y: 400
-	});
-
-	console.log(mousePosition);
-
-	useEffect(() => {
-		const onMouseMove = (e: MouseEvent) => {
-			const {clientX, clientY} = e;
-			setMousePosition({x: clientX, y: clientY});
-		};
-
-		window.addEventListener('mousemove', onMouseMove);
-
-		return () => {
-			window.removeEventListener('mousemove', onMouseMove);
-		};
-	}, []);
-
-	return (
-		<></>
-	)
+interface CustomCursorProps {
+    text?: string;
+    cursorColor?: string;
+    textColor?: string;
 }
+
+const CustomCursor: React.FC<CustomCursorProps> = ({
+    text = "Developer", // Default text
+    cursorColor = "#6c5ce7", // Default cursor color (purple)
+    textColor = "#fff" // Default text color (white)
+}) => {
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const onMouseMove = (e: MouseEvent) => {
+            setMousePosition({ x: e.clientX, y: e.clientY });
+        };
+
+        window.addEventListener("mousemove", onMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMove);
+        };
+    }, []);
+
+    return (
+		<div
+			className="custom-cursor"
+			style={{
+				top: mousePosition.y,
+				left: mousePosition.x,
+				color: textColor,
+			}}
+		>
+			<svg className="size-3.5" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
+				<path fill={cursorColor} d="M19.438 6.716 1.115.05A.832.832 0 0 0 .05 1.116L6.712 19.45a.834.834 0 0 0 1.557.025l3.198-8 7.995-3.2a.833.833 0 0 0 0-1.559h-.024Z"></path>
+			</svg>
+			<div
+				style={{
+					backgroundColor: cursorColor,
+				}}
+			>{text}</div>
+		</div>
+	);
+};
 
 export default CustomCursor;
