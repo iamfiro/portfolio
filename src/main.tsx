5478683211d1fa20a6ec150f28fa805b11bc12b8
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-21 21:42:19 +0900

    refactor: home.tsx router에 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import PageHome from "./pages/home.tsx";
 
 const router = createBrowserRouter([
-
+	{
+		path: '/',
+		element: <PageHome />
+	}
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+const PageHome = () => {
+	return (
+		<>
+			<h1>Home</h1>
+			<p>Welcome to the home page!</p>
+		</>
+	)
+}
+
+export default PageHome;
