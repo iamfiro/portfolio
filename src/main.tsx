8f945bc7f391d7a0e8e2aadb15e53d6c8eb3d066
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-21 21:39:22 +0900

    refactor: main.tsx react-router-dom 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+		<RouterProvider router={router} />
     </React.StrictMode>,
 )
