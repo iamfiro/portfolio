3c5659c81004789d4435be62534a2f428ecd7882
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-22 13:23:10 +0900

    feat: home.tsx 헤더 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
-import {CustomCursor} from "../components";
+import {CustomCursor, Header} from "../components";
 import useMousePosition from "../hooks/useMouseEvent.ts";
 
 const PageHome = () => {
 	const mousePosition = useMousePosition();
 	return (
 		<>
+			<Header />
 			<h1>Home</h1>
 			<p>Welcome to the home page!</p>
 			<CustomCursor text={'Anonymous'} x={mousePosition.x} y={mousePosition.y} />
