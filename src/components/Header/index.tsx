0e7793901ed287af139d29dfee90c5de8e3c5640
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-22 13:22:35 +0900

    feat: 헤더 컴포넌트 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import style from './style.module.scss';
+
+const Header = () => {
+	return (
+		<header className={style.contai}>
+			<h1>header</h1>
+		</header>
+	)
+};
+
+export default Header;
