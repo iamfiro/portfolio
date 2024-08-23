2c998ff3180f5175573bec729abe0c42984ef69e
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-23 23:47:00 +0900

    feat: project.ts에 프로젝트 추가

--- src/constant/project.ts
+++ src/constant/project.ts
@@ -1,3 +1,10 @@
+import SunrinToday from '../assets/image/sunrin-today.webp';
+import SunrinTodayBlack from '../assets/image/sunrin-today_black.png';
+import NoDream from '../assets/image/nodream.png';
+import NoDreamBlack from '../assets/image/nodream_black.png';
+import Watch from '../assets/image/watch.webp';
+import WatchBlack from '../assets/image/watch_black.png';
+
 export interface Project {
 	type: 'project' | 'design' | 'other';
 	name: string;
@@ -10,11 +17,27 @@ export interface Project {
 export const ProjectList: Project[] = [
 	{
 		type: 'project',
+		name: '노드림 Nodream',
+		year: 2024,
+		thumbnails: NoDreamBlack,
+		thumbnails2: NoDream,
+		href: '/project/nodream',
+	},
+	{
+		type: 'project',
 		name: '선린투데이',
 		year: 2024,
-		thumbnails: 'https://source.unsplash.com/random/270x170',
-		thumbnails2: 'https://source.unsplash.com/random/270x170',
+		thumbnails: SunrinTodayBlack,
+		thumbnails2: SunrinToday,
 		href: '/project/sunrintoday',
+	},
+	{
+		type: 'design',
+		name: '스마트 워치 디자인 - WatchOS',
+		year: 2024,
+		thumbnails: WatchBlack,
+		thumbnails2: Watch,
+		href: '/project/watch',
 	}
 ]
 
