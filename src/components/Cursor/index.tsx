5739d2fa78d5c851f979c606a386ebb8ec7c8956
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-23 15:42:08 +0900

    refactor: 커스텀 커서 닉네임 요소 삭제

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -10,7 +10,7 @@ interface CustomCursorProps {
 
 const CustomCursor: React.FC<CustomCursorProps> = ({
     text = "Developer", // Default text
-    cursorColor = "#6c5ce7", // Default cursor color (purple)
+    cursorColor = "#000", // Default cursor color (purple)
     textColor = "#fff", // Default text color (white)
 	x,
 	y,
@@ -21,17 +21,12 @@ const CustomCursor: React.FC<CustomCursorProps> = ({
 			style={{
 				top: y,
 				left: x,
-				color: textColor,
 			}}
 		>
 			<svg className="size-3.5" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 20 20">
 				<path fill={cursorColor} d="M19.438 6.716 1.115.05A.832.832 0 0 0 .05 1.116L6.712 19.45a.834.834 0 0 0 1.557.025l3.198-8 7.995-3.2a.833.833 0 0 0 0-1.559h-.024Z"></path>
 			</svg>
-			<div
-				style={{
-					backgroundColor: cursorColor,
-				}}
-			>{text}</div>
+			{/*<div>{text}</div>*/}
 		</div>
 	);
 };
