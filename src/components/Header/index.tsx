ce27d8f51b99fdbdd863f35b5d412479fb84c82b
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-23 11:14:39 +0900

    feat: 스크롤시 border-bottom width가 0으로 바뀌게 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,38 @@
 import style from './style.module.scss';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Logo from '../../assets/logo_flower.svg';
 
 const Header = () => {
 	const [time, setTime] = useState('00:00');
+	const [isScrolled, setIsScrolled] = useState(false);
 
 	// 시간 업데이트
-	setInterval(() => {
-		const date = new Date();
-		setTime(`${date.getHours()}:${date.getMinutes()}`);
-	}, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const date = new Date();
+			setTime(`${date.getHours()}:${date.getMinutes()}`);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	// 스크롤 감지 및 border-width 변경
+	useEffect(() => {
+		const handleScroll = () => {
+			setIsScrolled(window.scrollY !== 0);
+		};
+
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
 	return (
-		<header className={style.container}>
+		<header className={`${style.container}`} style={{ borderBottomWidth: isScrolled ? '0px' : '1px' }}>
 			<span className={style.title}>@devfiro</span>
 			<img src={Logo} alt="logo" className={style.logo}/>
 			<span className={style.time}>Seoul, Korea {time}</span>
 		</header>
-	)
+	);
 };
 
 export default Header;
-
