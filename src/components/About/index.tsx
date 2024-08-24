12f2fc2ffb09725f78010864be13b2f01d06c5d6
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-24 23:10:39 +0900

    feat: About 모토 추가

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,15 +3,14 @@ import {useLayoutEffect} from "react";
 import SplitType from "split-type";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { FaUserAlt, FaBookOpen } from "react-icons/fa";
+import { FaPenNib } from "react-icons/fa6";
 
 const About = () => {
 	gsap.registerPlugin(ScrollTrigger);
 
 	useLayoutEffect(() => {
-		new SplitType('#about_content', {
-			// types: 'lines',
-			// lineClass: 'char',
-		});
+		new SplitType('#about_content');
 
 		gsap.fromTo('#about_content .char', {
 			y: 100,
@@ -27,6 +26,12 @@ const About = () => {
 				start: 'top 90%',
 			}
 		});
+
+		const symbolTl = gsap.timeline({paused: false, repeat: -1});
+
+		symbolTl.to('#iam_title_symbol', {duration: 1, opacity: .7});
+		symbolTl.to('#iam_title_symbol', {duration: .5, opacity: .3});
+		symbolTl.to('#iam_title_symbol', {duration: 1, opacity: .7});
 	}, []);
 
 	return (
@@ -46,13 +51,22 @@ const About = () => {
 			<hr className={style.divider} />
 			<section className={style.iamContainer}>
 				<div>
-					<span className={style.iamTitle}>제가 생각하는 저는...</span>
+					<span className={style.iamTitleSymbol} id={'iam_title_symbol'}>●</span>
+					<span className={style.iamTitle}>제가 생각하는 개발자는...</span>
 				</div>
 				<div>
 					<p className={style.iamContent}>
-						프론트엔드 개발자로서 사용자 중심의 서비스를 만들기 위해 노력하고 있습니다<br/>
-						새로운 기술에 대한 지식을 쌓는 것을 즐기며<br/>
-						최신 기술을 통해 사용자에게 즐거움을 주는 서비스를 만들고 싶습니다
+						<strong><FaUserAlt /> 사용자 친화적을 중요하게 생각합니다</strong>
+						일상생활에서 불편하게 느끼는 부분을 해결하는 서비스를 만들려고 노력하고 있습니다
+					</p>
+					<p className={style.iamContent}>
+						<strong><FaPenNib /> 뭐든지 기록하려고 노력하고 있어요</strong>
+						내가 배웠던 기술을 열심히 기록하며<br/>
+						과거의 나를 통해 한발짝 도약하는 사람이 되고 싶습니다
+					</p>
+					<p className={style.iamContent}>
+						<strong><FaBookOpen /> 끊임없이 새로운 기술을 배우는 것을 중요하게 생각합니다</strong>
+						빠르게 바뀌는 개발 시장에서 뒤쳐지지 않게 꾸준히 새로운 기술을 배우고 있습니다
 					</p>
 				</div>
 			</section>
