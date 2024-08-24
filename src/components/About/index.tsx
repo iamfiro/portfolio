9edb6c49016f190942a4f7de9ef1ddd33b051a43
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-24 21:38:41 +0900

    refactor: About 컴포넌트 디자인 변경

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,47 +1,47 @@
 import style from './style.module.scss';
-import {Row} from "../index.ts";
-import {SocialList} from "../../constant/social.ts";
-import { MdArrowUpward } from "react-icons/md";
-import {FaGithub, FaInstagram} from "react-icons/fa";
-import { IoMailOpenOutline } from "react-icons/io5";
-
-function SocialTitleToIcon(title: string) {
-	switch (title) {
-		case 'Github':
-			return <FaGithub />
-		case 'Instagram':
-			return <FaInstagram />
-		default:
-			return <IoMailOpenOutline />;
-	}
-}
+import {useLayoutEffect} from "react";
+import SplitType from "split-type";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { IoMdFlower } from "react-icons/io";
 
 const About = () => {
+	gsap.registerPlugin(ScrollTrigger);
+
+	useLayoutEffect(() => {
+		new SplitType('#about_content', {
+			// types: 'lines',
+			// lineClass: 'char',
+		});
+
+		gsap.fromTo('#about_content .char', {
+			y: 100,
+			opacity: 0
+		}, {
+			y: 0,
+			opacity: .8,
+			duration: .5,
+			stagger: .02,
+			ease: 'power1',
+			scrollTrigger: {
+				trigger: '#about_content',
+				start: 'top 90%',
+			}
+		});
+	}, []);
+
 	return (
 		<div className={style.container}>
-			<section className={style.socialContainer}>
-				<Row className={style.socialSection}>
-					<span className={style.socialTitle}>( Social )</span>
-				</Row>
-				{
-					SocialList.map((social, index) => (
-						<a href={social.link} target={'_blank'} key={index} className={`${style.socialSection} ${style.socialItem}`} style={{justifyContent: 'space-between'}}>
-							<span className={style.socialName}>{SocialTitleToIcon(social.name)} {social.name}</span>
-							<span className={style.socialLink}>바로가기 <MdArrowUpward size={21} /></span>
-						</a>
-					))
-				}
-			</section>
-			<section className={style.aboutContainer}>
-				<span className={style.aboutTitle}>About Me</span>
-				<Row className={style.aboutContent}>
-					<p>
-						<span>(About Me)</span>
-						프론트엔드 개발자 조성주임 ㅇㅇ 저는 쌈@뽕한 스킬을 가지고 있고 누구든 뿅가는 개발자가 되고 싶어요. 그리고 여러분 제가 무슨 말을 하는지 모르겠는데 일단 그냥 들으세요
-						ㅋ
-					</p>
-				</Row>
-			</section>
+			<div style={{overflowY: 'hidden'}} id={"about_title_container"}>
+				<span id={"about_title"} className={style.aboutTitle}>About Me</span>
+			</div>
+			<p className={style.aboutContent} id={"about_content"}>
+				안녕하세요, 프론트엔드 개발자 조성주입니다<br/>
+				현재 선린인터넷고등학교에 재학 중이며<br/>
+				개발에 대한 열정을 가지고 있습니다<br/>
+				사람들에게 도움이 되는 서비스를 만들기 위해 꾸준히 노력하고 있으며<br/>
+				새로운 기술을 배우는 것을 즐깁니다.
+			</p>
 		</div>
 	)
 }
