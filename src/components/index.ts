3b2cb3b1c73627747a6f9db38dc79a879510f13b
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-24 17:50:47 +0900

    refactor: global import를 위해 index.ts에 About 컴포넌트 export default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as CustomCursor } from './Cursor';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
+export { default as About } from './About';
 
 // Common Components
 export { default as Row } from './common/Row';
