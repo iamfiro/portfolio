c02e021a0ec626680f030c15a695934e09792c66
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-26 08:10:04 +0900

    refactor: global import를 위해 index.ts에 Footer 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Header } from './Header';
 export { default as Hero } from './Hero';
 export { default as About } from './About';
 export { default as Stack } from './Stack';
+export { default as Footer } from './Footer';
 
 // Common Components
 export { default as Row } from './common/Row';
