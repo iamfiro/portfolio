55c3c5145c08c40d2c89490213948836cf9e1ca5
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-25 18:15:10 +0900

    feat: Stack 컴포넌트 Parallax 애니메이션 구현

--- src/components/Stack/index.tsx
+++ src/components/Stack/index.tsx
@@ -1,7 +1,43 @@
 import style from './style.module.scss';
+import { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const Stack = () => {
+	const containerRef = useRef<HTMLDivElement>(null);
 
+	gsap.registerPlugin(ScrollTrigger);
+
+	useLayoutEffect(() => {
+		if(!containerRef.current) return;
+
+		const tl = gsap.timeline();
+
+		tl.to(containerRef.current, {
+			scrollTrigger: {
+				trigger: containerRef.current,
+				start: 'top bottom',
+				end: 'top top',
+				scrub: 1,
+				markers: true,
+				onEnter: () => console.log('enter'),
+				onLeave: () => console.log('leave'),
+			},
+			marginTop: `-${window.innerHeight}px`,
+			ease: 'none',
+		});
+	}, []);
+
+	return (
+		<section className={style.container} style={{ position: 'relative', zIndex: 1 }}>
+			<div className={`${style.container} stories_container`} ref={containerRef}>
+				a
+			</div>
+			<div className={style.nextSection} style={{ zIndex: 0 }}>
+				{/* 다음 섹션의 콘텐츠 */}
+			</div>
+		</section>
+	);
 };
 
 export default Stack;
