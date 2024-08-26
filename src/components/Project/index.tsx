c990af263b965ea96cf536b5981707d6b02ba537
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-26 15:34:09 +0900

    fix: index.tsx 타입 에러

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -8,78 +8,88 @@ import TitleIcon from '../../assets/image/svg/Projects.svg';
 
 const Project = () => {
 	const titleRef = useRef<HTMLImageElement>(null);
-	const projectRefs = useRef<HTMLDivElement[]>([]);
+	const projectRefs = useRef<(HTMLImageElement | null)[]>([]);
 
 	useLayoutEffect(() => {
 		gsap.registerPlugin(ScrollTrigger);
 
-		gsap.to(titleRef.current, {
-			scrollTrigger: {
-				trigger: titleRef.current,
-				start: 'bottom bottom+=100px',
-				end: 'center center',
-				scrub: 1,
-			},
-			ease: 'power1.inOut',
-			width: '10%',
-		});
+		if (titleRef.current) {
+			gsap.to(titleRef.current, {
+				scrollTrigger: {
+					trigger: titleRef.current,
+					start: 'bottom bottom+=100px',
+					end: 'center center',
+					scrub: 1,
+				},
+				ease: 'power1.inOut',
+				width: '10%',
+			});
+		}
 
-		gsap.fromTo('.project_list', {
-			opacity: 0,
-			y: 100,
-		}, {
-			opacity: 1,
-			y: 0,
-			duration: .8,
-			stagger: .2,
-			scrollTrigger: {
-				trigger: titleRef.current,
-				start: 'top center',
-				end: 'center center',
+		gsap.fromTo(
+			'.project_list',
+			{
+				opacity: 0,
+				y: 100,
 			},
-		});
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				stagger: 0.2,
+				scrollTrigger: {
+					trigger: titleRef.current!,
+					start: 'top center',
+					end: 'center center',
+				},
+			}
+		);
 	}, []);
 
 	const handleMouseEnter = (index: number) => {
-		gsap.to(projectRefs.current[index], {
-			clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-			duration: 0.3,
-		});
+		const projectRef = projectRefs.current[index];
+		if (projectRef) {
+			gsap.to(projectRef, {
+				clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+				duration: 0.3,
+			});
+		}
 	};
 
 	const handleMouseLeave = (index: number) => {
-		gsap.to(projectRefs.current[index], {
-			clipPath: 'polygon(0 0, 0% 0, 0% 100%, 0% 100%)',
-			duration: 0.3,
-		});
+		const projectRef = projectRefs.current[index];
+		if (projectRef) {
+			gsap.to(projectRef, {
+				clipPath: 'polygon(0 0, 0% 0, 0% 100%, 0% 100%)',
+				duration: 0.3,
+			});
+		}
 	};
 
 	return (
 		<section className={style.container}>
 			<img src={TitleIcon} alt="Project" className={style.title} ref={titleRef}/>
 			<Column className={style.projectList}>
-				{
-					ProjectList.map((project, index) => (
-						<article
-							className={`${style.project} project_list`}
-							key={index}
-							onMouseEnter={() => handleMouseEnter(index)}
-							onMouseLeave={() => handleMouseLeave(index)}
-						>
-							<Column className={style.projectContent} style={{gap: '10px'}}>
-								<span className={style.projectYear}>( {project.year} )</span>
-								<span>{project.name}</span>
-							</Column>
-							<img
-								src={project.thumbnails2}
-								alt={project.name}
-								className={style.projectImage}
-								ref={el => projectRefs.current[index] = el}
-								style={{ clipPath: 'polygon(0 0, 0% 0, 0% 100%, 0% 100%)' }}
-							/>
-						</article>
-					))
-				}
+				{ProjectList.map((project, index) => (
+					<article
+						className={`${style.project} project_list`}
+						key={index}
+						onMouseEnter={() => handleMouseEnter(index)}
+						onMouseLeave={() => handleMouseLeave(index)}
+					>
+						<Column className={style.projectContent} style={{gap: '10px'}}>
+							<span className={style.projectYear}>( {project.year} )</span>
+							<span>{project.name}</span>
+						</Column>
+						<img
+							src={project.thumbnails2}
+							alt={project.name}
+							className={style.projectImage}
+							ref={el => projectRefs.current[index] = el}
+							style={{clipPath: 'polygon(0 0, 0% 0, 0% 100%, 0% 100%)'}}
+						/>
+					</article>
+				))}
 			</Column>
 		</section>
 	);
