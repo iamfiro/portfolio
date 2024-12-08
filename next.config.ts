ccfc2f60b92bbfd363d2d908fca2e305fafd5006
iamfiro <appturbo102@gmail.com>
2024-12-08 22:29:43 +0900

    scss 개쩌는 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    sassOptions: {
+        additionalData: `@use "src/styles/variables.scss" as *;`,
+    },
 };
 
 export default nextConfig;
