ff0090da3520640d6f062711fe36b9a19f2d8dcb
iamfiro <appturbo102@gmail.com>
2024-12-08 23:15:35 +0900

    이잉이이 스타일이 왜 적용이 안되너너너ㅓ너ㅓ5

--- src/components/@Home/Hero/index.tsx
+++ src/components/@Home/Hero/index.tsx
@@ -27,7 +27,7 @@ const HomeHero = () => {
                             <IoMdArrowUp/>
                         </div>
                     </article>
-                    <Link href={'/blog/human-by-ai-persona'} className={style.recentBlog}>
+                    <div href={'/blog/human-by-ai-persona'} className={style.recentBlog}>
                         <div className={style.category}>
                             <BsRssFill />
                             <span>최근 블로그</span>
@@ -43,7 +43,7 @@ const HomeHero = () => {
                             인공지능과 사람 지능의 인가관계에 대해 좀 더 자세히 살펴보면 다음과 같습니다.
                             인공지능은 사람의 학습능력, 추론능력, 문제해결능력 등을 모방하여 구현합니다. 즉, 사람이 경험과 학습을 통해 지식을 쌓아가고 이를 바탕으로 문제를 해결해 나가는 과정을 컴퓨터 프로그램으로 구현하는 것이 인공지능의 핵심입니다.
                         </span>
-                    </Link>
+                    </div>
                 </section>
             </div>
         </div>
