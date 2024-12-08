7bccfe6de4b331248e0d69aed9d074a8c67a9f9f
iamfiro <appturbo102@gmail.com>
2024-12-08 20:17:55 +0900

    MaxWidth 컴포넌트 삭제

--- src/components/MaxWidth/MaxWidth.tsx
+++
@@ -1,8 +0,0 @@
-import style from './MaxWidth.module.css';
-import {ReactNode} from "react";
-
-const MaxWidth = ({ children }: {children: ReactNode}) => {
-    return <div className={style.maxWidth}>{children}</div>;
-}
-
-export default MaxWidth;
