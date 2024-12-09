ae207f4bd5120af4ad41bcb5692eba5797d8a730
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-12-09 09:11:52 +0900

    SmoothScroll `ReferenceError: document is not defined` 오류 해결

--- src/components/SmoothScroll/SmoothScroll.tsx
+++ src/components/SmoothScroll/SmoothScroll.tsx
@@ -5,6 +5,8 @@ import LocomotiveScroll from 'locomotive-scroll';
 
 const SmoothScroll = ({children}: { children: ReactNode }) => {
 	useEffect(() => {
+		if(typeof window === 'undefined' || !document) return;
+
 		const scroll = new LocomotiveScroll({
 			el: document.querySelector('body') as HTMLBodyElement,
 			smooth: true
