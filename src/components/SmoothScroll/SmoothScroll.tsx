37460aa0ff048fb0377ac87208c4e71bc7d4dbf4
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-12-13 08:07:26 +0900

    스크롤 관련 요소가 잘 못 배치되는 버그가 있어 SmoothScroll.tsx 삭제

--- src/components/SmoothScroll/SmoothScroll.tsx
+++ src/components/SmoothScroll/SmoothScroll.tsx
@@ -4,18 +4,18 @@ import {ReactNode, useEffect} from "react";
 import LocomotiveScroll from 'locomotive-scroll';
 
 const SmoothScroll = ({children}: { children: ReactNode }) => {
-	useEffect(() => {
-		if(typeof window === 'undefined' || !document) return;
-
-		const scroll = new LocomotiveScroll({
-			el: document.querySelector('body') as HTMLBodyElement,
-			smooth: true
-		});
-
-		return () => {
-			scroll.destroy();
-		};
-	}, []);
+	// useEffect(() => {
+	// 	if(typeof window === 'undefined' || !document) return;
+	//
+	// 	const scroll = new LocomotiveScroll({
+	// 		el: document.querySelector('body') as HTMLBodyElement,
+	// 		smooth: true
+	// 	});
+	//
+	// 	return () => {
+	// 		scroll.destroy();
+	// 	};
+	// }, []);
 
 	return children;
 }
