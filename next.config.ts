6ae0b4b04eb14a778f7ef1bba473f96932f64197
iamfiro <appturbo102@gmail.com>
2025-01-06 21:19:22 +0900

    revert: 프로젝트 갈아엎기 오홍홍^^

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    sassOptions: {
-        additionalData: `@use "src/styles/variables.scss" as *; @use "src/styles/mixins.scss" as *;`,
-    },
-};
-
-export default nextConfig;
