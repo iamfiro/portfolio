1fb238b82eab45fe6dc2efa74e9616ee40141e5e
iamfiro <appturbo102@gmail.com>
2025-01-10 19:31:00 +0900

    기본적인 scss 세팅; next.config.ts에 additionalData 속성으로 작성하는 모든 scss 파일에 자동으로 varialbles, mixins scss 파일이 import 되게 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	sassOptions: {
+		additionalData: `@use "styles/variables.scss" as *; @use "styles/mixins.scss" as *;`,
+	},
 };
 
 export default nextConfig;
