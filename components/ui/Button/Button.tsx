c868f7148aada23da23859c9b2ce5e302d25b7a3
iamfiro <appturbo102@gmail.com>
2025-01-21 17:14:11 +0900

    Button.tsx의 ref props @typescript-eslint/no-explicit-any 오류를 해결결

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -2,58 +2,63 @@ import { createElement, forwardRef, PropsWithChildren, Ref } from 'react';
 import s from './style.module.scss';
 
 interface ButtonBaseProps {
-    href?: string
+	href?: string;
 
 	variant?: 'primary' | 'secondary';
 	size?: 'sm' | 'md' | 'lg';
 
-    fullRadius?: boolean;
-    fullWidth?: boolean;
+	fullRadius?: boolean;
+	fullWidth?: boolean;
 
 	leadingIcon?: React.ReactNode;
 	trailingIcon?: React.ReactNode;
 
-	isLoading?: boolean; 
+	isLoading?: boolean;
 }
 
-type ButtonProps = ButtonBaseProps & PropsWithChildren
+type ButtonProps = ButtonBaseProps & PropsWithChildren;
 
 // @typescript-eslint/no-explicit-any
-const Button = forwardRef(function Button(props: ButtonProps, ref: Ref<any>) {
+const Button = forwardRef(function Button(
+	props: ButtonProps,
+	ref: Ref<HTMLButtonElement | HTMLAnchorElement>,
+) {
 	const {
 		variant = 'primary',
 		size = 'md',
-		leadingIcon, trailingIcon,
-        fullRadius, fullWidth,
+		leadingIcon,
+		trailingIcon,
+		fullRadius,
+		fullWidth,
 		children,
 		isLoading,
 		...rest
 	} = props;
 
-    const as = props.href ? 'a' : 'button';
-    const className = `${s.button} ${isLoading ? s.loading : ''} ${fullRadius ? s.fullRadius : ''} ${fullWidth ? s.fullWidth : ''}`;
-    const render = isLoading ? (
-        <span className={s.loader} />
-    ) : (
-        <>
-            {leadingIcon && leadingIcon}
-            {children}
-            {trailingIcon && trailingIcon}
-        </>
-    )
+	const as = props.href ? 'a' : 'button';
+	const className = `${s.button} ${isLoading ? s.loading : ''} ${fullRadius ? s.fullRadius : ''} ${fullWidth ? s.fullWidth : ''}`;
+	const render = isLoading ? (
+		<span className={s.loader} />
+	) : (
+		<>
+			{leadingIcon && leadingIcon}
+			{children}
+			{trailingIcon && trailingIcon}
+		</>
+	);
 
 	return createElement(
-        as,
-        {
-            ref,
-            className,
-            'data-variant': variant,
-            'data-size': size,
-            disabled: as === 'button' ? isLoading : undefined,
-            ...rest
-        },
-        render
-    )
+		as,
+		{
+			ref,
+			className,
+			'data-variant': variant,
+			'data-size': size,
+			disabled: as === 'button' ? isLoading : undefined,
+			...rest,
+		},
+		render,
+	);
 });
 
 export default Button;
