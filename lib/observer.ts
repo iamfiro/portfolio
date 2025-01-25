bac15cad652448a79d3cd3ff52489e463f257b5b
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-01-25 23:04:44 +0900

    Table Of Content 기능 개발

---
+++ lib/observer.ts
@@ -0,0 +1,37 @@
+import { Dispatch, SetStateAction } from 'react';
+
+const observerOption = {
+	threshold: 0.3,
+	rootMargin: '-120px 0px 0px 0px',
+};
+
+export const getIntersectionObserver = (
+	setState: Dispatch<SetStateAction<string>>,
+) => {
+	let direction = 'down';
+	let lastScrollY = 0;
+
+	// scroll direction check
+	const checkDirection = (prevY: number) => {
+		const scrollY = window.scrollY;
+		if (!(prevY && scrollY)) return;
+
+		direction = scrollY > lastScrollY ? 'down' : 'up'; // 이전의 스크롤 위치와 비교하기
+		lastScrollY = scrollY; // 현재의 스크롤 값을 저장
+	};
+
+	const observer = new IntersectionObserver((entries) => {
+		checkDirection(lastScrollY);
+
+		entries.forEach((entry) => {
+			if (
+				(direction === 'down' && entry.isIntersecting) ||
+				(direction === 'up' && entry.isIntersecting)
+			) {
+				setState(entry.target.id);
+			}
+		});
+	}, observerOption);
+
+	return observer;
+};
