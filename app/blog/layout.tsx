6b57bf58c9a1146f90ec43481da13149c95bd3fb
iamfiro <appturbo102@gmail.com>
2025-08-23 19:58:19 +0900

    new design

--- app/blog/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-const BlogLayout = ({children}: PropsWithChildren) => {
-    return children;
-}
-
-export default BlogLayout;
--- lib/date.ts
+++
@@ -1,4 +0,0 @@
-export function getMonthAndDay(date: string) {
-    const d = new Date(date);
-    return `${d.getMonth() + 1}. ${d.getDate()}.`;
-}
